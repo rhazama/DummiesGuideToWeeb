156f8fcfcb5f1db885d724a5f5312195ba2e973a
TAY-HERN <hernandeztaylor1@gmail.com>
2022-03-27 16:17:48 -0700

    add Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import logo from './assets/images/logo.png';
+import react from 'react';
+import Homepage from './pages/Homepage.js';
+import User from './pages/User.js';
+import Anime from './pages/Anime.js';
 import './App.css';
 
 function App() {
