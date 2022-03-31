90cd05cfcfef977a055d447c085f3dd711e5e639
rhazama <rhazama01@gmail.com>
2022-03-31 14:24:53 -0700

    added user password functions

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const animeSchema = require('./anime');
 
-const userSchema = new Schema{
+const userSchema = new Schema(
     {
         username:{
             type: String,
@@ -12,7 +12,7 @@ const userSchema = new Schema{
         },
         email:{
             type: String,
-            required: true;
+            required: true,
             unique: true,
             match: [/.+@.+\..+/, 'Must use a valid email address'],
         },
@@ -27,9 +27,26 @@ const userSchema = new Schema{
             virtuals:true,
         },
     }
+);
+
+// has the user password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// custom method to compare and validate the password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
 };
 
-
+// when we get a query a user, we will also get another field called 'animeCount' with the number of save saved books we have
+userSchema.virtual('animeCount').get(function () {
+    return this.savedAnime.length;
+});
 
 const User = model('User', userSchema);
 
