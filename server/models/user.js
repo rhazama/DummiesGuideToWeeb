f80f5055d7ca36e368ad442b0c4900185376b740
rhazama <rhazama01@gmail.com>
2022-03-31 14:50:48 -0700

    changed formatting

--- server/models/user.js
+++ server/models/user.js
@@ -32,7 +32,8 @@ const userSchema = new Schema(
 // has the user password
 userSchema.pre('save', async function(next) {
     if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;this.password = await bcrypt.hash(this.password, saltRounds);
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
     }
 
     next();
