213a8e0818aa231a3b387bc54a6a64b32d4961ff
rhazama <rhazama01@gmail.com>
2022-03-31 18:10:40 -0700

    Added typeDefs under Schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        animeCount: Int!
+        savedAnime: [anime]
+    }
+    type Anime {
+        animeId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input AnimeInput {
+        authors: [String]
+        description: String!
+        animeId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    type: Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, passwrod: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeAnime(animeId: ID!): User
+    }
+    `;
+
+module.exports = typeDefs;
